40ef5f640942f3bad2f8b2f388bb1212f4ef8eb1
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-26 00:37:35 +0100

    add filter

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+function fetchCountries(countryName) {
+    return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
     .then(
     (response) => {
       if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import './css/styles.css';
 import { fetchCountries } from "./fetchCountries";
 import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 3000;
+const DEBOUNCE_DELAY = 1000;
 
 const inputEl = document.querySelector('input#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onNameInput, DEBOUNCE_DELAY));
-
 function onNameInput(event){
     const countryName = event.target.value.trim()
     fetchCountries(countryName)
@@ -19,13 +18,18 @@ function onNameInput(event){
 }
 function renderCountryCard(countries) {
     const markup = countries
-      .map(({capital, population, languages}) => {
+      .map(({name, flags, capital, population, languages}) => {
         return `
+            <div class="country-card">
+            <h2><img src="${flags.svg}" alt="${flags.alt} width="40" height="20""> ${name.official}</h2>
             <p><b>Capital:</b> ${capital}</p>
             <p><b>Population:</b> ${population}</p>
             <p><b>Languages:</b> ${Object.values(languages)}</p>
-            `;
+            </div>
+            `
       })
       .join("");
       countryInfoEl.innerHTML = markup;
   }
+
+
