724a29b12e57885272784f08f5b5409d968a4c22
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-27 02:23:09 +0200

    add css & edit js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,13 @@
-function fetchCountries(countryName) {
-    return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
-    .then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error("response.status");
-      }
-      return response.json();
-    }
-  );
-}
+import Notiflix from 'notiflix';
 
+function fetchCountries(countryName) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
+}
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries";
+import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 1000;
 
@@ -9,27 +10,55 @@ const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onNameInput, DEBOUNCE_DELAY));
-function onNameInput(event){
-    const countryName = event.target.value.trim()
-    fetchCountries(countryName)
-    .then(renderCountryCard)
-    .catch(error => console.log("Щось пішло не так"))
-    console.log(countryName);
-}
-function renderCountryCard(countries) {
-    const markup = countries
-      .map(({name, flags, capital, population, languages}) => {
-        return `
-            <div class="country-card">
-            <h2><img src="${flags.svg}" alt="${flags.alt} width="40" height="20""> ${name.official}</h2>
-            <p><b>Capital:</b> ${capital}</p>
-            <p><b>Population:</b> ${population}</p>
-            <p><b>Languages:</b> ${Object.values(languages)}</p>
-            </div>
-            `
-      })
-      .join("");
-      countryInfoEl.innerHTML = markup;
+
+function onNameInput(event) {
+  const countryName = event.target.value.trim();
+  countryInfoEl.innerHTML = '';
+  countryListEl.innerHTML = '';
+  if (countryName === '') {
+    return;
   }
+  fetchCountries(countryName).then(response => {
+    console.log('Знайдено часткових співпадінь:', response.length);
+    if (response.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    } else if (response.length > 1 && response.length <= 10) {
+      renderSearchList(response);
+    } else if (response.length === 1) {
+      renderCountryCard(response);
+    }
+  });
+}
 
+function renderSearchList(countries) {
+  const markupList = countries
+    .map(({ name, flags }) => {
+      return `
+            <li class="country-item">
+            <img src="${flags.svg}" alt="${flags.alt} width="40" height="20""><h3>${name.official}</h3>
+            </li>
+            `;
+    })
+    .join('');
+  countryListEl.innerHTML = markupList;
+}
 
+function renderCountryCard(countries) {
+  const markupCard = countries
+    .map(({ name, flags, capital, population, languages }) => {
+      return `
+        <div class="country-card">
+        <h2><img src="${flags.svg}" alt="${
+        flags.alt
+      } width="40" height="20""> ${name.official}</h2>
+        <p><b>Capital:</b> ${capital}</p>
+        <p><b>Population:</b> ${population}</p>
+        <p><b>Languages:</b> ${Object.values(languages)}</p>
+        </div>
+        `;
+    })
+    .join('');
+  countryInfoEl.innerHTML = markupCard;
+}
