a0f04fdccda7c80148083de4ec9feb8e4ff4af62
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-27 02:40:00 +0200

    edit debounce

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('input#search-box');
 const countryListEl = document.querySelector('.country-list');
